1020f92629b39c295e251e29c74e35c28b50970a
andersbor <anbo@easj.dk>
2017-09-07 13:35:02 +0200

    Redundant code remove from interface

--- currencyService2/currencyService2/ICurrencyService.cs
+++ currencyService2/currencyService2/ICurrencyService.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Runtime.Serialization;
 using System.ServiceModel;
 
 namespace currencyService2
@@ -12,27 +11,5 @@ namespace currencyService2
 
         [OperationContract]
         ICollection<Currency> GetAllCurrencyInfos();
-    }
-
-    // Use a data contract as illustrated in the sample below to add composite types to service operations.
-    [DataContract]
-    public class CompositeType
-    {
-        bool boolValue = true;
-        string stringValue = "Hello ";
-
-        [DataMember]
-        public bool BoolValue
-        {
-            get { return boolValue; }
-            set { boolValue = value; }
-        }
-
-        [DataMember]
-        public string StringValue
-        {
-            get { return stringValue; }
-            set { stringValue = value; }
-        }
     }
 }
