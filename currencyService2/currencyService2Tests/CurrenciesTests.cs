09e68aa4d3d062da9cbf740c857d19c6ea01cd0c
andersbor <anbo@easj.dk>
2017-09-07 12:07:51 +0200

    Test intentionally fails

--- currencyService2/currencyService2Tests/CurrenciesTests.cs
+++ currencyService2/currencyService2Tests/CurrenciesTests.cs
@@ -19,6 +19,8 @@ namespace currencyService2.Tests
             Assert.AreEqual(1.126, rate, 0.01);
             rate = cur.Rate(CurrencyCode.SEK, CurrencyCode.EUR);
             Assert.AreEqual(0.104, rate, 0.01);
+
+            Assert.Fail();
         }
     }
 }
